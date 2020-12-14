942d32c52b25135a3376b990bdefc6a9e1daf605
Alex Hamilton <alex.b.hamilton56@gmail.com>
2020-12-13 22:10:00 -0500

    connects to databases for get routes

--- index.js
+++ index.js
@@ -1,62 +1,31 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
-const cors = require('cors');
 
-const sampleShow = {
-  date: new Date(),
-  venue: 'Logan Street Studio',
-  sets: {
-    setOne: [
-      {
-        id: 1,
-        title: 'Johnny'
-      },
-      {
-        id: 2,
-        title: 'O'
-      },
-      {
-        id: 3,
-        title: 'Go Funk Yourself'
-      }
-    ],
-    setTwo: [
-      {
-        id: 4,
-        title: 'Peace Lily'
-      },
-      {
-        id: 5,
-        title: 'Head 2 Head'
-      },
-      {
-        id: 6,
-        title: 'Tyler\'s Song'
-      }
-    ],
-    encore: [
-      {
-        id: 7,
-        title: 'Diggin a Hole'
-      },
-      {
-        id: 8,
-        title: 'Johnny (reprise)'
-      }
-    ]
-  }
-}
-;
-
-app.use(cors());
+// mongoose model imports
+const Song = require('./models/Song');
+const Show = require('./models/Show');
 
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/api/show', (request, response) => {
-  response.json(sampleShow);
+app.get('/api/shows', (request, response) => {
+  Show
+    .find({})
+    .then(shows => {
+      console.log(shows);
+      response.json(shows);
+    });
+});
+
+app.get('/api/songs', (request, response) => {
+  Song
+    .find({})
+    .then(songs => {
+      response.json(songs);
+    });
 });
 
 app.get('*', (req, res) => {
