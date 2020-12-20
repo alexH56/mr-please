ecea6b5d3930743f19eaa31b3f44a32bb352692e
Alex Hamilton <alex.b.hamilton56@gmail.com>
2020-12-20 16:43:52 -0500

    added new params to connection

--- models/Show.js
+++ models/Show.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
-const url = process.env.MONGO_MAIN_URI;
+const url = process.env.MONGODB_URI;
 
 console.log('connecting to', url);
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
   .then(result => {
-    console.log('connected to MongoDB');
+    console.log('connected to Show db');
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message);
+    console.log('error connecting to Show db:', error.message);
   });
 
 const showSchema = new mongoose.Schema({
