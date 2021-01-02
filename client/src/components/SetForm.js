8410cd8769f32ac3b6b67161569a34f7597c9d95
Alex Hamilton <alex.b.hamilton56@gmail.com>
2021-01-01 23:09:59 -0500

    created SetForm to add new shows to db

---
+++ client/src/components/SetForm.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+const SetForm = ({ songs }) => {
+  const [newShow, setNewShow] = useState([]);
+
+  const handleSongSelect = (e) => {
+    console.log(e.target.value);
+    let newSong;
+    for (const song of songs) {
+      if (song.title === e.target.value) {
+        newSong = song;
+      }
+    }
+    console.log(newSong);
+    const showCopy = newShow.concat(newSong);
+    setNewShow(showCopy);
+  };
+
+  return (
+    <>
+      <h1>New Show:</h1>
+      <p>{newShow.map(song => song.title).join(', ')}</p>
+
+      <form>
+        <select name='song' value='' onChange={handleSongSelect}>
+          <option value=''>-add a song here-</option>
+          {songs ? songs.map(song => <option key={song.title} value={song.title}>{song.title}</option>) : null}
+        </select>
+      </form>
+    </>
+  );
+};
+
+export default SetForm
+;
