1d63685d5517877603581c9785cf3dd3e2b82d98
Alex Hamilton <alex.b.hamilton56@gmail.com>
2021-01-03 17:22:36 -0500

    removed _id from returns

--- models/Song.js
+++ models/Song.js
@@ -22,6 +22,7 @@ songSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject.__v;
+    delete returnedObject._id;
   }
 });
 
