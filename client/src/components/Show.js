739f0d71f1611c6349a25ae36a033e63f4424da9
Alex Hamilton <alex.b.hamilton56@gmail.com>
2021-01-03 17:26:27 -0500

    renamed info -> show, changed conditionals  to read length

--- client/src/components/Show.js
+++ client/src/components/Show.js
@@ -1,30 +1,40 @@
 import React from 'react';
 
-const Show = ({ info }) => {
-  const setOne = info.sets.setOne;
-  const setTwo = info.sets.setTwo;
-  const encore = info.sets.encore;
+const Show = ({ show }) => {
+  const date = Object.values(show.date).join('/');
+  const venueName = show.venue.name;
 
-  const date = Object.values(info.date).join('/');
+  const setOne = show.sets[1];
+  const setTwo = show.sets[2];
+  const setThree = show.sets[3];
+  const encore = show.sets.encore;
+
+  const showNotes = show.showNotes;
 
   return (
-
     <div>
-      <h3>{date} - {info.venue.name}</h3>
+      <h3>{date} - {venueName}</h3>
 
-      {setOne
+      {setOne.length > 0
         ? <p>Set One: {setOne.map(song => song.title).join(', ')}</p>
         : null}
 
-      {setTwo
+      {setTwo.length > 0
         ? <p>Set Two: {setTwo.map(song => song.title).join(', ')}</p>
         : null}
 
-      {encore
+      {setThree.length > 0
+        ? <p>Set Three: {setThree.map(song => song.title).join(', ')}</p>
+        : null}
+
+      {encore.length > 0
         ? <p>Encore: {encore.map(song => song.title).join(', ')}</p>
         : null}
-    </div>
 
+      {showNotes
+        ? <p>Notes: {showNotes}</p>
+        : null}
+    </div>
   );
 };
 
