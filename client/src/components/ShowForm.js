0602633cce4082a390168f78b8acd19d0ad4cf8b
Alex Hamilton <alex.b.hamilton56@gmail.com>
2021-01-03 17:25:24 -0500

    created ShowForm to add shows to database

--- client/src/components/ShowForm.js
+++ client/src/components/ShowForm.js
@@ -1,9 +1,55 @@
 import React, { useState } from 'react';
 
-const ShowForm = ({ songs }) => {
-  const [newShow, setNewShow] = useState([]);
+import SetForm from './SetForm';
 
-  const handleSongSelect = (e) => {
+const ShowForm = ({ songs }) => {
+  const blankSet = {
+    date: {
+      month: 1,
+      day: 1,
+      year: 2001
+    },
+    venue: {
+      name: '',
+      location: {
+        street: '',
+        street2: '',
+        city: '',
+        state: '',
+        country: ''
+      }
+    },
+    sets: {
+      1: [],
+      2: [],
+      3: [],
+      encore: []
+    },
+    showNotes: '',
+    countsForStats: true,
+    audioLink: ''
+  };
+  const [newShow, setNewShow] = useState(blankSet);
+  const [counter, setCounter] = useState(1);
+  const [encoreClicked, setEncoreClicked] = useState(false);
+
+  const renderSetForms = (counter) => {
+    const content = [];
+    for (let i = 1; i <= counter; i++) {
+      content.push(
+        <SetForm
+          key={i}
+          setID={i}
+          songs={songs}
+          newShow={newShow}
+          handleSongSelect={handleSongSelect}
+        />
+      );
+    }
+    return content;
+  };
+
+  const handleSongSelect = (e, setID) => {
     console.log(e.target.value);
     let newSong;
     for (const song of songs) {
@@ -11,23 +57,50 @@ const ShowForm = ({ songs }) => {
         newSong = song;
       }
     }
-    console.log(newSong);
-    const showCopy = newShow.concat(newSong);
-    setNewShow(showCopy);
+    const newSet = newShow.sets[setID].concat(newSong);
+    setNewShow({ ...newShow, sets: { ...newShow.sets, [setID]: newSet } },
+      console.log(newShow));
   };
 
   return (
-    <>
+    <div>
       <h1>New Show:</h1>
-      <p>{newShow.map(song => song.title).join(', ')}</p>
 
-      <form>
-        <select name='song' value='' onChange={handleSongSelect}>
-          <option value=''>-add a song here-</option>
-          {songs ? songs.map(song => <option key={song.title} value={song.title}>{song.title}</option>) : null}
-        </select>
-      </form>
-    </>
+      {renderSetForms(counter)}
+
+      {encoreClicked
+        ? <SetForm
+          key='encore'
+          setID='encore'
+          songs={songs}
+          newShow={newShow}
+          handleSongSelect={handleSongSelect}
+        />
+        : null}
+
+      {counter < 3
+        ? <button onClick={() => setCounter(counter + 1)}>Add Set</button>
+        : null}
+
+      {counter > 1
+        ? <button onClick={() => {
+          setNewShow({ ...newShow, sets: { ...newShow.sets, [counter]: [] } });
+          setCounter(counter - 1);
+        }}
+        >
+        Remove Set
+        </button>
+        : null}
+
+      <button onClick={() => {
+        setNewShow({ ...newShow, sets: { ...newShow.sets, encore: [] } });
+        setEncoreClicked(!encoreClicked);
+      }}
+      >
+        {encoreClicked ? 'Remove Encore' : 'Add Encore'}
+      </button>
+
+    </div>
   );
 };
 
