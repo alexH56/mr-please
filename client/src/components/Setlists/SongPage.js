59580b46d90e6cb0f5aec817b86ec19fbd1eb274
Alex Hamilton <alex.b.hamilton56@gmail.com>
2021-02-25 00:17:07 -0500

    logic to find/display shows containing song

--- client/src/components/Setlists/SongPage.js
+++ client/src/components/Setlists/SongPage.js
@@ -2,12 +2,49 @@ import React from 'react';
 
 import { useParams } from 'react-router-dom';
 
+import Show from './Show';
+
 const SongPage = ({ songs, shows }) => {
-  const { id } = useParams();
-  const song = songs.filter(song => song.URLname === id)[0];
+  const { songName } = useParams();
+  const song = songs
+    ? songs.filter(song => song.URLname === songName)[0]
+    : null;
+
+  const showsWithSong = shows && song
+    ? shows.filter(show => {
+      let match = false;
+      for (const set in show.sets) {
+        if (show.sets[set].some(song => song.URLname === songName)) {
+          match = true;
+          break;
+        }
+      }
+      return match;
+    })
+    : null;
+
+  //   console.log(showsWithSong);
 
   return (
-    <h1>{song.title}</h1>
+    <>
+      {song
+        ? <>
+          <h1>{song.title}</h1> <p>Has been played at the following shows:</p>
+          </>
+
+        : null}
+
+      {showsWithSong
+        ? <>
+          {showsWithSong.map(show => (
+            <Show
+              key={show.id}
+              show={show}
+            />
+          ))}
+          </>
+        : null}
+    </>
   );
 };
 
