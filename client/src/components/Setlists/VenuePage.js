7eaec10f02d2b3e199854f7308c2dbca32ff9c51
Alex Hamilton <alex.b.hamilton56@gmail.com>
2021-02-27 20:34:31 -0500

    added logic to display list of shows at venue

--- client/src/components/Setlists/VenuePage.js
+++ client/src/components/Setlists/VenuePage.js
@@ -2,12 +2,41 @@ import React from 'react';
 
 import { useParams } from 'react-router-dom';
 
+import Show from './Show';
+
 const VenuePage = ({ venues, shows }) => {
-  const { id } = useParams();
-  const venue = venues.filter(venue => venue.URLname === id)[0];
+  const { venueName } = useParams();
+  const venue = venues
+    ? venues.filter(venue => venue.URLname === venueName)[0]
+    : null;
+
+  const showsWithVenue = shows && venue
+    ? console.log(shows)
+    // shows.filter(show => show.venue.URLname === venueName)
+    : null;
+
+  console.log(showsWithVenue);
 
   return (
-    <h1>{venue.name}</h1>
+    <>
+      {venue
+        ? <>
+          <h1>{venue.name}</h1> <p>Has hosted the following shows:</p>
+        </>
+
+        : null}
+      {/*
+      {showsWithVenue
+        ? <>
+          {showsWithVenue.map(show => (
+            <Show
+              key={show.id}
+              show={show}
+            />
+          ))}
+          </>
+        : null} */}
+    </>
   );
 };
 
