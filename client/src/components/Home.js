3e338a066963ec3881f8c179cce85387d8ab00d8
Alex Hamilton <alex.b.hamilton56@gmail.com>
2021-04-14 00:49:15 -0400

    moved nav / altered landing-page message

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Home = () => {
   return (
-    <>
-      <nav>
-        <Link to='/setlists'>Setlists</Link>
-      </nav>
-
-      <h1>Hey what's up we're Mr. Please</h1>
-    </>
+    <main>
+      <h1>Hey, we're Mr. Please!</h1>
+      <h3>The site is very much under construction, but for now you can view our setlists by viewing the links above</h3>
+    </main>
   );
 };
 
