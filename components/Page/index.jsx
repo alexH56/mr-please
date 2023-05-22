5e1d2e8d0024f05ee6359e3e3e3268c8634255a5
alexH56 <alex.b.hamilton56@gmail.com>
2023-05-22 19:23:01 -0400

    built page component

---
+++ components/Page/index.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable react/forbid-prop-types */
+/* eslint-disable react/require-default-props */
+/* eslint-disable react/jsx-one-expression-per-line */
+import React from 'react';
+
+import Head from 'next/head';
+import PropTypes from 'prop-types';
+
+import Navbar from '../Navbar';
+
+export default function Page({ title, children }) {
+    return (
+        <>
+            <Head>
+                <title>Mr. Please - {title}</title>
+            </Head>
+
+            <Navbar />
+
+            <main className="flex justify-center p-12">
+                <div className="flex flex-col w-full max-w-screen-xl">
+                    {children}
+                </div>
+            </main>
+        </>
+    );
+}
+
+Page.propTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.any,
+};
