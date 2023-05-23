34eebe6efe925da1a8299616330ed96db1643c9a
alexH56 <alex.b.hamilton56@gmail.com>
2023-05-22 21:57:56 -0400

    added external link for store

--- components/Navbar/MenuItems.js
+++ components/Navbar/MenuItems.js
@@ -1,7 +1,7 @@
 const MenuItems = [
     { href: '/shows', text: 'Shows' },
     { href: '/music', text: 'Music' },
-    { href: '/matches', text: 'Videos' },
-    { href: '/store', text: 'Store' },
+    { href: '/videos', text: 'Videos' },
+    { href: 'https://mr-please.square.site/', text: 'Store', external: true },
 ];
 export default MenuItems;
