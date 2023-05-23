5a0c138b5a696a82863ae58b5b63a451abab9c21
alexH56 <alex.b.hamilton56@gmail.com>
2023-05-22 22:05:18 -0400

    added video link

--- components/Navbar/MenuItems.js
+++ components/Navbar/MenuItems.js
@@ -1,7 +1,11 @@
 const MenuItems = [
-    { href: '/shows', text: 'Shows' },
-    { href: '/music', text: 'Music' },
-    { href: '/videos', text: 'Videos' },
+    { href: '/shows', text: 'Shows', external: false },
+    { href: '/music', text: 'Music', external: false },
+    {
+        href: 'https://www.youtube.com/@mrpleasemusic',
+        text: 'Videos',
+        external: true,
+    },
     { href: 'https://mr-please.square.site/', text: 'Store', external: true },
 ];
 export default MenuItems;
