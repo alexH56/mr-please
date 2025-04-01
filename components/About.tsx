0c0650855ee1213385495cb03e15d7df836001b3
alexH56 <alex.b.hamilton56@gmail.com>
2025-03-31 23:03:28 -0400

    updated bio

--- components/About.tsx
+++ components/About.tsx
@@ -1,29 +1,64 @@
-"use client";
-
+'use client';
 
 export default function About() {
-  return (
-    <section id="about">
-      <div className="bg-[#6133B8] py-12">
-        <div className="container mx-auto px-4">
-          <h2 className="text-4xl font-bold mb-8 text-center text-[#F8F2FF]">About</h2>
-          <div className="max-w-3xl mx-auto text-lg text-[#F8F2FF]">
-            <p className="mb-4">
-              Mr. Please is a genre-defying musical experience from Louisville,
-              Kentucky. Drawing on a wide range of influences, from the
-              bluegrass of their native Kentucky to psychedelia, funk, and indie
-              rock, Mr. Please offers something for everyone with a sound
-              uniquely their own.
-            </p>
-            <p>
-              The band&apos;s dynamic live performances and innovative
-              songwriting have earned them a dedicated following, as they
-              continue to push boundaries and explore new musical territories
-              while staying true to their roots.
-            </p>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section id="about">
+			<div className="bg-[#6133B8] pt-6 pb-12">
+				<div className="container mx-auto px-4">
+					<div className="flex flex-col gap-4 max-w-4xl mx-auto text-lg text-[#F8F2FF]">
+						<h1 className="sr-only">About</h1>
+						<i className="text-2xl mb-2">
+							There's a duality at the heart of Mr. Please: a band driven by a
+							shared love for both the power of a great song and the magic of
+							live improvisation.
+						</i>
+						<p>
+							Drawing on a wide range of influences, the group weaves a sonic
+							tapestry that defies easy categorization. Blending their funk and
+							indie rock foundation with sounds from across the world and back
+							again to those of their native Kentucky, their innovative
+							songwriting and dynamic performance have earned them spots at
+							Summer Camp and Backwoods Music Festivals, along with opening
+							slots for acts like SunSquabi and Dizgo.
+						</p>
+						<p>
+							<b>In the studio,</b> the five-piece tackles subjects from the
+							serious to sexy to the absurd (and sometimes all at once). Not
+							content to limit themselves in style or genre, the group's only
+							goal is to craft songs with infectious melodies, memorable lyrics,
+							and grooves that make you want to 🕺 <i>dance</i> 💃.
+						</p>
+						<p>
+							<b>On the stage,</b> this no-holds-barred approach rings twice as
+							true. Minor-key electronica over a bluegrass beat? Jazzed-up
+							psychedelia on a 90's hip-hop cover? All on the table. The band
+							treats their live set as a playground where they expand on their
+							songs with jams that evolve every time they play, exchanging
+							energy with the audience in a way that leaves no two shows the
+							same.
+						</p>
+						<p>
+							In early 2020, the group was formed by a twist of fate. Guitarist
+							& vocalist <b>Max Voorhees</b> was living abroad in Asia and,
+							while on a visit home to Louisville, gathered friends{' '}
+							<b>Ryan Chadwick</b> (drums/vocals), <b>Alex Hamilton</b>{' '}
+							(guitar/vocals), and <b>Jarrod McClellan</b> (bass/vocals) for a
+							jam session. Days later, all flights back were canceled due to the
+							Covid-19 pandemic, and what was supposed to be a one-off jam
+							quickly became a serious project. After the world opened back up
+							and they began playing shows, the gang soon added{' '}
+							<b>Nico Extra</b> (keyboard/vocals) to round out their lineup.
+						</p>
+						<p>
+							In the years since, the band has built a dedicated fanbase as they
+							continue to push boundaries and explore new sonic territory. Hot
+							on the heels of their debut album <i>Pleasant Tense</i>, Mr.
+							Please is hitting the road to build their following—and
+							community—one jam at a time.
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
