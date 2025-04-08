a8f8f74b3d3f5c72b1b45f9d41848c140ca88733
alexH56 <alex.b.hamilton56@gmail.com>
2025-04-08 13:51:24 -0400

    deleted everything for clean slate

--- components/PleaseHead/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import Image from 'next/image';
-import pleaseHead from '../../public/please_head.png';
-
-export function PleaseHead() {
-    return <Image src={pleaseHead} width="48" height="48" />;
-}
--- utils/strings.js
+++
@@ -1,4 +0,0 @@
-// use URL constructor and return hostname
-export const getHostname = (url) => new URL(url).hostname;
-
-export const lipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus ut gravida nulla, sit amet luctus diam.';
